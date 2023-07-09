54cd2703dc604baea5780873b1aef7a86f7528ce
Hemachandra006 <p.hemachandra06@gmail.com>
2023-07-09 17:33:31 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").onclick = function () {
+    if(document.getElementById("cntsd").style.display == "block"){
+    document.getElementById("cntsd").style.display = "none";
+}
+    else{
+        document.getElementById("cntsd").style.display = "block";
+
+    }
+}
